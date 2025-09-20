39c1f19d2dcd4c5ae405be129b4e4f70b5ad4db3
Minh Duong <git@whitehoodhacker.net>
2025-09-19 18:41:30 -0700

    Update footer links in quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,9 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "SIGPwny": "https://sigpwny.com",
+      "Discord": "https://sigpwny.com/discord",
+      "GitHub": "https://github.com/sigpwny/notes",
     },
   }),
 }
