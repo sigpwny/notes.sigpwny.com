1a1db3e9a9797fa2cb3a47e72fea7f1c44970642
Minh Duong <git@whitehoodhacker.net>
2025-09-19 19:33:51 -0700

    Update condition to check for README case insensitively

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -20,7 +20,7 @@ export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ConditionalRender({
       component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "readme",
+      condition: (page) => page.fileData.slug.toUpperCase() !== "README",
     }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
